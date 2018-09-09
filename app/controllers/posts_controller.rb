c8684629e90ce3763662dde5d9df53bf5825fa9d
timhdavis13 <timhdavis13@gmail.com>
2018-09-08 18:26:54 -0700

    added create new Post feature
    -added a new Post form.
    -added Post model (with "rails g model Post title:string body:text").
    -created Post table in default database (SQLite) (with "rake db:migrate" after generating Post model).
    -added a 'new' method that allows the user to create a new Post that is saved to the database.
    -added a 'show' method to Posts to view an indiviual post.
    -creating a new post redirects the user to the Show Post page.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,35 @@
 class PostsController < ApplicationController
 
 def index
-    
+    #
+end
+
+def show
+    # Create the 'post' instance variable that the view will use:
+    @post = Post.find(params[:id]);
+end
+
+def new
+    #
+end
+
+def create
+    # For testing form successful: render plain: params[:post].inspect
+
+    # Create a 'post' instance variable & set to a new Post with
+    #  the specified parameters defined by the 'post_params' method (defined below):
+    @post = Post.new(post_params);
+
+    # Save the new Post in the DB and redirect to the show view for this Post:
+    @post.save;
+    redirect_to @post;
+end
+
+# Private methods:
+
+private def post_params
+    # Need to specify what parameters a Post can accept:
+    params.require(:post).permit(:title, :body);
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # Define the root:
   root 'posts#index'
 
+  # Define normal routes:
   get 'about' => 'pages#about'
+
+  # Define Resources: Maps HTTP verbs to controller actions:
+  resources :posts
 
 end
