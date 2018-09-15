b59e5ca23790ddfebcd6764152a53443efb10277
timhdavis13 <timhdavis13@gmail.com>
2018-09-15 11:29:29 -0700

    added create and show Comments
    -added a 'create' method in CommentsController.
    -added a form for adding comments to a post in the Show Post page.
    -added a list of the current comments on a post in the Show Post page.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+
+    def create
+
+        # Create a 'post' instance variable:
+        @post = Post.find(params[:post_id]);
+
+        # Create a 'comment' variable within the post variable:
+        @comment = @post.comments.create(comment_params);
+
+        redirect_to post_path(@post);
+    end
+
+
+    # Private methods:
+
+    private def comment_params
+        params.require(:comment).permit(:username, :body);
+    end
+
 end
